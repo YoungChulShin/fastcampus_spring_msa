277f79b65c72862c22ce78c7af74d7750ccf367f
YoungChulShin <go1323@gmail.com>
2021-08-15 22:11:43 +0900

    Partner service impl

--- order/src/main/java/dev/practice/order/domain/partner/PartnerInfo.java
+++ order/src/main/java/dev/practice/order/domain/partner/PartnerInfo.java
@@ -1,5 +1,24 @@
 package dev.practice.order.domain.partner;
 
+import dev.practice.order.domain.partner.Partner.Status;
+import lombok.Getter;
+
+@Getter
 public class PartnerInfo {
 
+  private Long id;
+  private String partnerToken;
+  private String partnerName;
+  private String businessNo;
+  private String email;
+  private Status status;
+
+  public PartnerInfo(Partner partner) {
+    this.id = partner.getId();
+    this.partnerToken = partner.getPartnerToken();
+    this.partnerName = partner.getPartnerName();
+    this.businessNo = partner.getBusinessNo();
+    this.email = partner.getEmail();
+    this.status = partner.getStatus();
+  }
 }
--- order/src/main/java/dev/practice/order/domain/partner/PartnerService.java
+++ order/src/main/java/dev/practice/order/domain/partner/PartnerService.java
@@ -2,4 +2,8 @@ package dev.practice.order.domain.partner;
 
 public interface PartnerService {
 
+  PartnerInfo registerPartner(PartnerCommand command);
+  PartnerInfo getPartnerInfo(String partnerToken);
+  PartnerInfo enablePartner(String partnerToken);
+  PartnerInfo disablePartner(String partnerToken);
 }
