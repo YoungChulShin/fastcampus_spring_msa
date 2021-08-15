d09302ad425991f3997edf53223ed6a13a6ee081
YoungChulShin <go1323@gmail.com>
2021-08-16 00:03:52 +0900

    Partner controller

---
+++ order/src/main/java/dev/practice/order/common/response/CommonControllerAdvice.java
@@ -0,0 +1,71 @@
+package dev.practice.order.common.response;
+
+import com.google.common.collect.Lists;
+import dev.practice.order.common.exception.BaseException;
+import dev.practice.order.common.interceptor.CommonHttpRequestInterceptor;
+import java.util.List;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.catalina.connector.ClientAbortException;
+import org.slf4j.MDC;
+import org.springframework.core.NestedExceptionUtils;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class CommonControllerAdvice {
+
+  private static final List<ErrorCode> SPECIFIC_ALERT_TARGET_ERROR_CODE_LIST = Lists.newArrayList();
+
+  @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+  @ExceptionHandler(value = Exception.class)
+  public CommonResponse onException(Exception e) {
+    String eventId = MDC.get(CommonHttpRequestInterceptor.HEADER_REQUEST_UUID_KEY);
+    log.error("eventId={}", eventId, e);
+    return CommonResponse.fail(ErrorCode.COMMON_SYSTEM_ERROR);
+  }
+
+  @ResponseStatus(HttpStatus.OK)
+  @ExceptionHandler(value = BaseException.class)
+  public CommonResponse onBaseException(BaseException e) {
+    String eventId = MDC.get(CommonHttpRequestInterceptor.HEADER_REQUEST_UUID_KEY);
+    log.error("eventId={}", eventId, e);
+    if (SPECIFIC_ALERT_TARGET_ERROR_CODE_LIST.contains(e.getErrorCode())) {
+      log.error("[BaseException] eventId = {}, cause = {}, errorMsg = {}", eventId, NestedExceptionUtils
+          .getMostSpecificCause(e), NestedExceptionUtils.getMostSpecificCause(e).getMessage());
+    } else {
+      log.warn("[BaseException] eventId = {}, cause = {}, errorMsg = {}", eventId, NestedExceptionUtils.getMostSpecificCause(e), NestedExceptionUtils.getMostSpecificCause(e).getMessage());
+    }
+    return CommonResponse.fail(e.getMessage(), e.getErrorCode().name());
+  }
+
+  @ResponseStatus(HttpStatus.OK)
+  @ExceptionHandler(value = {ClientAbortException.class})
+  public CommonResponse skipException(Exception e) {
+    String eventId = MDC.get(CommonHttpRequestInterceptor.HEADER_REQUEST_UUID_KEY);
+    log.warn("[skipException] eventId = {}, cause = {}, errorMsg = {}", eventId, NestedExceptionUtils.getMostSpecificCause(e), NestedExceptionUtils.getMostSpecificCause(e).getMessage());
+    return CommonResponse.fail(ErrorCode.COMMON_SYSTEM_ERROR);
+  }
+
+  @ResponseStatus(HttpStatus.BAD_REQUEST)
+  @ExceptionHandler(value = {MethodArgumentNotValidException.class})
+  public CommonResponse methodArgumentNotValidException(MethodArgumentNotValidException e) {
+    String eventId = MDC.get(CommonHttpRequestInterceptor.HEADER_REQUEST_UUID_KEY);
+    log.warn("[BaseException] eventId = {}, errorMsg = {}", eventId, NestedExceptionUtils.getMostSpecificCause(e).getMessage());
+    BindingResult bindingResult = e.getBindingResult();
+    FieldError fe = bindingResult.getFieldError();
+    if (fe != null) {
+      String message = "Request Error" + " " + fe.getField() + "=" + fe.getRejectedValue() + " (" + fe.getDefaultMessage() + ")";
+      return CommonResponse.fail(message, ErrorCode.COMMON_INVALID_PARAMETER.name());
+    } else {
+      return CommonResponse.fail(ErrorCode.COMMON_INVALID_PARAMETER.getErrorMsg(), ErrorCode.COMMON_INVALID_PARAMETER.name());
+    }
+  }
+
+}
