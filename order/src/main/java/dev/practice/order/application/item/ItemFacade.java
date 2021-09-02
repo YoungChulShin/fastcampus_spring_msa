55962d918380df3a51063251b0ffe4d8d312938d
YoungChulShin <go1323@gmail.com>
2021-09-03 06:00:28 +0900

    ItemFacade 구현

---
+++ order/src/main/java/dev/practice/order/application/item/ItemFacade.java
@@ -0,0 +1,34 @@
+package dev.practice.order.application.item;
+
+import dev.practice.order.domain.item.ItemCommand;
+import dev.practice.order.domain.item.ItemInfo;
+import dev.practice.order.domain.item.ItemService;
+import dev.practice.order.domain.notification.NotificationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ItemFacade {
+
+  private final ItemService itemService;
+  private final NotificationService notificationService;
+
+  public String registerItem(ItemCommand.RegisterItemRequest request, String partnerToken) {
+    var itemToken = itemService.registerItem(request, partnerToken);
+    notificationService.sendEmail(null, null, null);
+    return itemToken;
+  }
+
+  public void changeOnSale(String itemToken) {
+    itemService.changeOnSale(itemToken);
+  }
+
+  public void changeEndOfSale(String itemToken) {
+    itemService.changeEndOfSale(itemToken);
+  }
+
+  public ItemInfo.Main retrieveItemInfo(String itemToken) {
+    return itemService.retrieveItemInfo(itemToken);
+  }
+}
