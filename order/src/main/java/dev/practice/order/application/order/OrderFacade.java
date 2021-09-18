0f70c70dd7e897f8d379400698997e959279fbe0
YoungChulShin <go1323@gmail.com>
2021-09-18 09:26:03 +0900

    order facade 구현

--- order/src/main/java/dev/practice/order/application/order/OrderFacade.java
+++ order/src/main/java/dev/practice/order/application/order/OrderFacade.java
@@ -1,5 +1,8 @@
 package dev.practice.order.application.order;
 
+import dev.practice.order.domain.notification.NotificationService;
+import dev.practice.order.domain.order.OrderCommand;
+import dev.practice.order.domain.order.OrderInfo;
 import dev.practice.order.domain.order.OrderService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -11,5 +14,20 @@ import org.springframework.stereotype.Service;
 public class OrderFacade {
 
   private final OrderService orderService;
+  private final NotificationService notificationService;
 
+  public String registerOrder(OrderCommand.RegisterOrder registerOrder) {
+    String orderToken = orderService.registerOrder(registerOrder);
+    notificationService.sendKakao("test", "content");
+    return orderToken;
+  }
+
+  public OrderInfo.Main retriveOrder(String orderToken) {
+    return orderService.retrieveOrder(orderToken);
+  }
+
+  public void paymentOrder(OrderCommand.PaymentRequest paymentRequest) {
+    orderService.paymentOrder(paymentRequest);
+    notificationService.sendKakao("test", "test");
+  }
 }
