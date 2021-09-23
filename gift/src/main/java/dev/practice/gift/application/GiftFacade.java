2bd631906802b48eea42420cc7cf710163d30b67
YoungChulShin <go1323@gmail.com>
2021-09-23 22:11:07 +0900

    Gift Facade 개발

---
+++ gift/src/main/java/dev/practice/gift/application/GiftFacade.java
@@ -0,0 +1,39 @@
+package dev.practice.gift.application;
+
+import dev.practice.gift.domain.gift.GiftCommand;
+import dev.practice.gift.domain.gift.GiftInfo;
+import dev.practice.gift.domain.gift.GiftService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class GiftFacade {
+
+  private final GiftService giftService;
+
+  public GiftInfo getOrder(String giftToken) {
+    log.info("getOrder giftToken = {}", giftToken);
+    return giftService.getGiftInfo(giftToken);
+  }
+
+  public GiftInfo registerOrder(GiftCommand.Register reuqest) {
+    var giftInfo = giftService.registerOrder(reuqest);
+    log.info("registerOrder orderToken = {}", giftInfo.getOrderToken());
+    return giftInfo;
+  }
+
+  public void requestPaymentProcessing(String giftToken) {
+    giftService.requestPaymentProcessing(giftToken);
+  }
+
+  public void completePayment(String orderToken) {
+    giftService.completePayment(orderToken);
+  }
+
+  public void acceptGift(GiftCommand.Accept request) {
+    giftService.acceptGift(request);
+  }
+}
